b70f31a58388bb40225a3220e27462e9e9e1c2e0
Davluka <Davluka_D@hotmail.com>
2018-11-26 22:51:24 -0600

    Update GeneticAlgorithm.h
    update

--- GeneticAlgorithm.h
+++ GeneticAlgorithm.h
@@ -1,21 +1,50 @@
 #ifndef GENETIC_ALGORITHM
 #define GENETIC_ALGORITHM
 
-typedef struct chromosome { float fitness; void* data; };
 typedef void* population;
 
-typedef struct genetic_algorithm
+typedef struct GenAlg
 {
     population current_generation;
     population next_generation;
-    float (*fitness)(void*);
-    population (*selection)(population);
-    population (*crossover)(population);
-    population (*mutation_)(population);
+    float crossover_rate;
+    float mutation__rate;
     int population_size;
     int chromosome_size;
+    float (*fitness)(void*);
+    population (*new_pop)(float (*)(void*),int,int);
+    population (*selection)(population);
+    population (*crossover)(population,float);
+    population (*mutation_)(population,float);
 };
 
+GenAlg new_genAL( population (*_selection)(population),population (*_crossover)(population,float),
+                population (*_mutation)(population,float),population (*_init_gene)(float (*)(void*),int,int),
+                float (*_fitness)(void*),float cross_s,float mut_s,int pop_size,int chrom_size)
+{
+    GenAlg temp_genetic;
+    temp_genetic.crossover_rate=cross_s;
+    temp_genetic.mutation__rate=mut_s;
+    temp_genetic.population_size=pop_size;
+    temp_genetic.chromosome_size=chrom_size;
+    temp_genetic.new_pop=_init_gene;
+    temp_genetic.selection=_selection;
+    temp_genetic.crossover=_crossover;
+    temp_genetic.mutation_=_mutation;
+    return temp_genetic;
+}
 
+population Evolve(GenAlg _GA,int max_generations)
+{
+    int generation=0;
+    _GA.current_generation = _GA.new_pop(_GA.fitness,_GA.population_size,_GA.chromosome_size);
+    for(generation=0;generation<max_generations;generation++)
+    {
+        _GA.next_generation = _GA.selection(_GA.current_generation);
+        _GA.next_generation = _GA.crossover(_GA.next_generation,_GA.crossover_rate);
+        _GA.current_generation = _GA.mutation_(_GA.next_generation,_GA.mutation__rate);
+    }
+    return _GA.current_generation;
+}
 
 #endif
